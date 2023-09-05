cfca4d676c31467c662fc25569bbc29e6fedd826
Landmaster <python0429@gmail.com>
2023-09-05 19:19:12 -0400

    add basic impl

---
+++ src/worker.js
@@ -0,0 +1,13 @@
+import { createBackup } from "@nora-soderlund/cloudflare-d1-backups";
+
+export default {
+	async scheduled(event, env, ctx) {
+		await createBackup(env.BTD6_INDEX_DB, env.BTD6_INDEX_BACKUP);
+		const files = await env.BTD6_INDEX_BACKUP.list({prefix: 'backups'});
+		if (files.objects.length > 800) {
+			await env.BTD6_INDEX_BACKUP.delete(
+				files.objects.slice(0, files.objects.length - 800).map(obj => obj.key)
+			);
+		}
+	},
+};
