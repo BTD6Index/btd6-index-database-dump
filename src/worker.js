165014aae3db1ed660988757cccbfc828f2269de
Landmaster <python0429@gmail.com>
2023-09-05 20:59:08 -0400

    use iso string for backups

--- src/worker.js
+++ src/worker.js
@@ -2,7 +2,9 @@ import { createBackup } from "@nora-soderlund/cloudflare-d1-backups";
 
 export default {
 	async scheduled(event, env, ctx) {
-		await createBackup(env.BTD6_INDEX_DB, env.BTD6_INDEX_BACKUP);
+		await createBackup(env.BTD6_INDEX_DB, env.BTD6_INDEX_BACKUP, {
+			fileName: `backups/${new Date().toISOString()}`
+		});
 		const files = await env.BTD6_INDEX_BACKUP.list({prefix: 'backups'});
 		if (files.objects.length > 800) {
 			await env.BTD6_INDEX_BACKUP.delete(
