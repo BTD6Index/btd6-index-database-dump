81ba3a95d3a2cbd94bcc3040af2fe95d1d8954f4
Landmaster <python0429@gmail.com>
2023-09-06 20:06:54 -0400

    decrease frequency/max files

--- src/worker.js
+++ src/worker.js
@@ -1,14 +1,16 @@
 import { createBackup } from "@nora-soderlund/cloudflare-d1-backups";
 
+const MAX_FILES = 150;
+
 export default {
 	async scheduled(event, env, ctx) {
 		await createBackup(env.BTD6_INDEX_DB, env.BTD6_INDEX_BACKUP, {
 			fileName: `backups/${new Date().toISOString()}.sql`
 		});
 		const files = await env.BTD6_INDEX_BACKUP.list({prefix: 'backups'});
-		if (files.objects.length > 200) {
+		if (files.objects.length > MAX_FILES) {
 			await env.BTD6_INDEX_BACKUP.delete(
-				files.objects.slice(0, files.objects.length - 200).map(obj => obj.key)
+				files.objects.slice(0, files.objects.length - MAX_FILES).map(obj => obj.key)
 			);
 		}
 	},
